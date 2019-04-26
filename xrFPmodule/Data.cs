7b12deee59876a1f0b299a2c68f9d199a0273b9d
alien-wxr-temp <49577496+alien-wxr-temp@users.noreply.github.com>
2019-04-26 18:19:12 +0800

    Update Data.cs

--- xrFPmodule/Data.cs
+++ xrFPmodule/Data.cs
@@ -25,15 +25,28 @@ namespace xrFPmodule
         // shared data
         public string folderPath;
         public string logPath;
+        public string tempName;
+
         public int EnrolledFingersMask = 0;
         public int MaxEnrollFingerCount = MaxTemplates;
         public bool IsEventHandlerSucceeds = true;
         public bool IsFeatureSetMatched = false;
         public int FalseAcceptRate = 0;
-        public DPFP.Template[] Templates = new DPFP.Template[MaxTemplates];
 
+        public DPFP.Template[] Templates = new DPFP.Template[MaxTemplates];
+        public int serialNum = 0;
+        public List<string> serialName = new List<string>();
+
+        public int userNum = 0;
+        public List<database> userList = new List<database>();
         // data change notification
         public void Update() { OnChange(); }        // just fires the OnChange() event
         public event OnChangeHandler OnChange;
+    }
+
+    public class database
+    {
+        public string username;
+        public int fpNum;
     }
 }
